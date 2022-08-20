09d00b663a864e18d463a98680c1655a716d4dde
zhuxuchen <2517870679@qq.com>
2022-08-20 16:05:06 +0800

    优化了SQL语句

--- src/main/java/com/ly/springboot/mapper/LoginMapper.java
+++ src/main/java/com/ly/springboot/mapper/LoginMapper.java
@@ -10,14 +10,20 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface LoginMapper {
     //管理员登录
-    @Select("select * from admin where adminId = #{username} and pwd = #{password}")
+    @Select("select adminId,adminName,sex,tel,email,cardId,role " +
+            "from admin " +
+            "where adminId = #{username} and pwd = #{password}")
     Admin adminLogin(@Param("username") Integer username ,@Param("password") String password);
 
     //教师登录
-    @Select("select * from teacher where teacherId = #{username} and pwd = #{password}")
+    @Select("select teacherId,teacherName,institute,sex,tel,email,cardId,type,role " +
+            "from teacher " +
+            "where teacherId = #{username} and pwd = #{password}")
     Teacher teacherLogin(@Param("username") Integer username ,@Param("password") String password);
 
     //学生登录
-    @Select("select * from student where studentId = #{username} and pwd = #{password}")
+    @Select("select studentId,studentName,grade,major,clazz,institute,tel,email,cardId,sex,role " +
+            "from student " +
+            "where studentId = #{username} and pwd = #{password}")
     Student studentLogin(@Param("username") Integer username ,@Param("password") String password);
 }

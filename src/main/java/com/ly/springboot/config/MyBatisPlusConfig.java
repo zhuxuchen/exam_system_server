f1ed141d25f0add05e1b70de3c1a2ef1eecaf0e0
zhuxuchen <2517870679@qq.com>
2022-09-16 15:40:00 +0800

    更改了配置

--- src/main/java/com/ly/springboot/config/MyBatisPlusConfig.java
+++ src/main/java/com/ly/springboot/config/MyBatisPlusConfig.java
@@ -1,4 +1,4 @@
-package com.ly.springboot;
+package com.ly.springboot.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
